0b7fce0748d50f902e4dbbfd1c33d685368a7d0d
James Bithell <james@bithell.com>
2020-11-30 18:30:43 +0000

    Change from 2x magnets and 1x reed switch to 1x magnet and 2x hall effect sensors

--- peppa/peppa.ino
+++ peppa/peppa.ino
@@ -1,24 +1,37 @@
 #include <WiFi.h>
 #include <HTTPClient.h>
+#include "time.h" //NTP Time
+#include <EEPROM.h>
+#define EEPROM_SIZE 1
 
 #include ".\config.h"
 
-#define SLOWEST_ROTATION 2000 //The maximum amount of time in ms it can take to go from one magnet to the other
-#define FASTEST_ROTATION 10
+#define SLOWEST_ROTATION 5000 //The maximum amount of time in ms it can take to do a complete lap
 #define UPLOAD_FREQUENCY 3600000
- 
-volatile int lastRotation1 = 0;
-volatile int lastRotation2 = 0;
+
+#define SENSOR1 4
+#define SENSOR2 5
+
+#define DSTOFFSET 3600
+
+//Internal Variables
+
+volatile int rotationStart = 0;
 volatile int awaiting = 1; //Which loop are we currently waiting for
-volatile bool lapInProgress = false;
 
 volatile int laps = 0;
 volatile int fastestLap = 9999;
 volatile unsigned long totalLapTime = 0;
 
-bool uploaded = false;
+const char* ntpServer1 = "pool.ntp.org";
+const char* ntpServer2 = "time.google.com";
+const char* ntpServer3 = "time.cloudflare.com";
 
-
+void disconnectWifi() {
+  WiFi.disconnect(true);
+  WiFi.mode(WIFI_OFF);
+  Serial.println("WiFi disconnected");
+}
 void connectWifi() {
   WiFi.begin(ssid, password);
   Serial.println("Connecting");
@@ -30,86 +43,129 @@ void connectWifi() {
   Serial.print("Connected to WiFi network with IP Address: ");
   Serial.println(WiFi.localIP());
 }
+
+int timeMinutes()
+{
+  struct tm timeinfo;
+  if(!getLocalTime(&timeinfo)){
+    Serial.println("Failed to obtain time");
+    return 99;
+  }
+  return timeinfo.tm_min;
+}
+int timeHours()
+{
+  struct tm timeinfo;
+  if(!getLocalTime(&timeinfo)){
+    Serial.println("Failed to obtain time");
+    return 99;
+  }
+  return timeinfo.tm_hour;
+}
+int timeDay()
+{
+  struct tm timeinfo;
+  if(!getLocalTime(&timeinfo)){
+    Serial.println("Failed to obtain time");
+    return 99;
+  }
+  return timeinfo.tm_mday;
+}
+int timeMonth()
+{
+  struct tm timeinfo;
+  if(!getLocalTime(&timeinfo)){
+    Serial.println("Failed to obtain time");
+    return 99;
+  }
+  return timeinfo.tm_mon;
+}
+
 void setup() {
-  pinMode(15, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(15), pulse, FALLING);
+  EEPROM.begin(EEPROM_SIZE);
+  
   Serial.begin(9600);
   Serial.println("Booted");
+
+  connectWifi();
+  configTime(0, DSTOFFSET, ntpServer1, ntpServer2,ntpServer3);
+  disconnectWifi();
+  
+  pinMode(SENSOR1, INPUT_PULLUP);
+  pinMode(SENSOR2, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(SENSOR1), pulseOne, RISING);
+  attachInterrupt(digitalPinToInterrupt(SENSOR2), pulseTwo, RISING);
 }
-void pulse() {
-  if (!lapInProgress) {
-      awaiting = 2;
-      lastRotation1 = millis();
-      lapInProgress = true;
-  } else {
-    if (awaiting == 2) {
-      //We've got the second magnet - time to wait for the 3rd
-      if ((millis()-lastRotation1) > SLOWEST_ROTATION || (millis()-lastRotation1) < FASTEST_ROTATION) {
-        awaiting = 2;
-        lastRotation1 = millis();
-        lapInProgress = true;
-      } else {
-        lastRotation2 = millis();
-        awaiting = 3;
+void pulseOne() {
+  if (awaiting == 3) { //The lap has just ended - and another one has begin  
+    if ((millis()-rotationStart) <= SLOWEST_ROTATION) { //Evaluate the timings of the last lap
+      //Store the lap
+      int thisTime = millis()-rotationStart;
+      if (thisTime < fastestLap) {
+        fastestLap = thisTime;
       }
-      
-    } else {
-      //The lap has just ended - and another one has begin
-      if ((millis()-lastRotation2) > SLOWEST_ROTATION || (lastRotation2-lastRotation1) > SLOWEST_ROTATION || (millis()-lastRotation2) < FASTEST_ROTATION) {
-        //Too fast or slow - reject it
-      } else {
-        //Store the lap
-        int thisTime = millis()-lastRotation1;
-        if (thisTime < fastestLap) {
-          fastestLap = thisTime;
-        }
-        totalLapTime += thisTime;
-        laps++;
-      }
-      
-      awaiting = 2;
-      lastRotation1 = millis();
-      lapInProgress = true;
-      
+      totalLapTime += thisTime;
+      laps++;
+      Serial.print("New lap ");
+      Serial.println(thisTime);
     }
   }
 
+  //Setup to expect the next pulse
+  awaiting = 2;
+  rotationStart = millis();
 }
+void pulseTwo() {
+  if (awaiting == 2) {
+    awaiting = 3; 
+  } else {
+    awaiting = 1;
+  }
+}
+
 void loop() {
-  if (millis() > UPLOAD_FREQUENCY && !uploaded) { //12 hours
-    detachInterrupt(digitalPinToInterrupt(15));
-    uploaded = true;
-    connectWifi();
-    if(WiFi.status()== WL_CONNECTED){
-      HTTPClient http;
-      
-      http.begin(serverName);
-      
-      http.addHeader("Content-Type", "application/x-www-form-urlencoded");
-      // Data to send with HTTP POST
-      String httpRequestData = "value1=" + String(totalLapTime) + "&value2=" + String(laps) + "&value3=" + String(fastestLap);           
-      // Send HTTP POST request
-      int httpResponseCode = http.POST(httpRequestData);
-      
-      /*
-      // If you need an HTTP request with a content type: application/json, use the following:
-      http.addHeader("Content-Type", "application/json");
-      // JSON data to send with HTTP POST
-      String httpRequestData = "{\"value1\":\"" + String(random(40)) + "\",\"value2\":\"" + String(random(40)) + "\",\"value3\":\"" + String(random(40)) + "\"}";
-      // Send HTTP POST request
-      int httpResponseCode = http.POST(httpRequestData);
-      */
-     
-      if (httpResponseCode != 200) {
-        Serial.print("FAIL     HTTP Response code: ");
-        Serial.println(httpResponseCode);
-      } else {
-        Serial.println("Success");
+  if (timeHours() > 5) { //6am
+    if (timeDay() != EEPROM.read(0)) { //The upload for today hasn't been done yet!
+      detachInterrupt(digitalPinToInterrupt(SENSOR1));
+      detachInterrupt(digitalPinToInterrupt(SENSOR2));
+      if (laps > 10 && fastestLap < 9999 && totalLapTime > 0) {
+        connectWifi();
+        if(WiFi.status()== WL_CONNECTED){
+          HTTPClient http;
+          
+          http.begin(serverName);
+          
+          http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+          // Data to send with HTTP POST
+          String httpRequestData = "value1=" + String(totalLapTime) + "&value2=" + String(laps) + "&value3=" + String(fastestLap);           
+          // Send HTTP POST request
+          int httpResponseCode = http.POST(httpRequestData);
+          
+          /*
+          // If you need an HTTP request with a content type: application/json, use the following:
+          http.addHeader("Content-Type", "application/json");
+          // JSON data to send with HTTP POST
+          String httpRequestData = "{\"value1\":\"" + String(random(40)) + "\",\"value2\":\"" + String(random(40)) + "\",\"value3\":\"" + String(random(40)) + "\"}";
+          // Send HTTP POST request
+          int httpResponseCode = http.POST(httpRequestData);
+          */
+         
+          if (httpResponseCode != 200) {
+            Serial.print("FAIL     HTTP Response code: ");
+            Serial.println(httpResponseCode);
+          } else {
+            Serial.println("Success");
+          }
+         
+          http.end();
+        }
       }
-     
-      http.end();
-      
+  
+      EEPROM.write(0, timeDay()); //Set the memory to today
+      EEPROM.commit();
       ESP.restart();
     }
   }
+
+  delay(5000);
 }
